68d5c39df2826a951f2cc5d7b903528ad7cac883
Undiess <nguyen.andy67@gmail.com>
2020-07-31 01:52:42 +0930

    Updates

---
+++ precache-manifest.671ef0a99ef8af59c4eeb451682efa5a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a650331d25bd646d6f7466fd6a1a3830",
+    "url": "/Undiess/react_portfolio/index.html"
+  },
+  {
+    "revision": "2826cf6dd909cb39effa",
+    "url": "/Undiess/react_portfolio/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "e84ec84349cd4794ed87",
+    "url": "/Undiess/react_portfolio/static/js/2.5c576b9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Undiess/react_portfolio/static/js/2.5c576b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2826cf6dd909cb39effa",
+    "url": "/Undiess/react_portfolio/static/js/main.1079b8d5.chunk.js"
+  },
+  {
+    "revision": "71742c856e8db5988e7d",
+    "url": "/Undiess/react_portfolio/static/js/runtime-main.6d29939d.js"
+  },
+  {
+    "revision": "6f2ddfab61d3daed332079c692d9acfd",
+    "url": "/Undiess/react_portfolio/static/media/IMG_1799.6f2ddfab.jpg"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/Undiess/react_portfolio/static/media/cpp.f278be79.png"
+  },
+  {
+    "revision": "bf104c5ef4b088b41d43ac12b25ac352",
+    "url": "/Undiess/react_portfolio/static/media/css.bf104c5e.png"
+  },
+  {
+    "revision": "1b5f300c3330800d356adabdcbd5154c",
+    "url": "/Undiess/react_portfolio/static/media/golfapp.1b5f300c.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/Undiess/react_portfolio/static/media/html.664a8c00.png"
+  },
+  {
+    "revision": "afb47678875386aa6e81b65aa0640563",
+    "url": "/Undiess/react_portfolio/static/media/javascipt.afb47678.png"
+  },
+  {
+    "revision": "8af8c7e3f7db665c7a729563a40ec98c",
+    "url": "/Undiess/react_portfolio/static/media/lodafones.8af8c7e3.png"
+  },
+  {
+    "revision": "4b12dd41635001837f4fe5f02de9a6eb",
+    "url": "/Undiess/react_portfolio/static/media/node.4b12dd41.png"
+  },
+  {
+    "revision": "14c89a41e5154b4a2920df0115337ad5",
+    "url": "/Undiess/react_portfolio/static/media/reactlogo.14c89a41.png"
+  },
+  {
+    "revision": "0d9b16f911d9f4d24c811105bd383422",
+    "url": "/Undiess/react_portfolio/static/media/swiftlogo.0d9b16f9.png"
+  },
+  {
+    "revision": "7aabd127b80bebf5dad3b0c9e07c0a97",
+    "url": "/Undiess/react_portfolio/static/media/vuelogo.7aabd127.png"
+  }
+]);
